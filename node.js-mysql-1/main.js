c08e2dff994aedcbf4aa2f429e33eda821fa5998
Aminoragit <wjsxkxk1@gmail.com>
2021-06-07 18:04:25 +0900

    update,delete query 추가

--- node.js-mysql-1/main.js
+++ node.js-mysql-1/main.js
@@ -5,9 +5,6 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-
-
-
 //1. mysql을 실행
 //2.db를 호출
 var mysql = require('mysql');
@@ -17,6 +14,7 @@ var db = mysql.createConnection({
   password: '0000',
   database: 'opentutorials'
 });
+//가장 중요
 db.connect();
 
 
@@ -132,10 +130,10 @@ var app = http.createServer(function(request, response) {
         var html = template.HTML(title, list,
           `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
               </p>
               <p>
                 <input type="submit">
@@ -155,18 +153,23 @@ var app = http.createServer(function(request, response) {
     });
     request.on('end', function() {
       var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-          console.log(result);
-          response.writeHead(302, {
-            Location: `/?id=${title}`
-          });
-          response.end();
-        })
-      });
+      // var id = post.id;
+      // var title = post.title;
+      // var description = post.description;
+      // fs.rename(`data/${id}`, `data/${title}`, function(error) {
+      //   fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+      //     console.log(result);
+      //     response.writeHead(302, {
+      //       Location: `/?id=${title}`
+      //     });
+      //     response.end();
+      //   })
+      // });
+
+      db.query('UPDATE topic SET title=?,description=?,author_id=1 WHERE id=?',[post.title,post.description,post.id],function(error,result){
+        response.writeHead(302,{Location: `/?id=${post.id}`});
+        response.end();
+      })
     });
   } else if (pathname === '/delete_process') {
     var body = '';
@@ -175,14 +178,21 @@ var app = http.createServer(function(request, response) {
     });
     request.on('end', function() {
       var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error) {
-        response.writeHead(302, {
-          Location: `/`
-        });
+      // var id = post.id;
+      // var filteredId = path.parse(id).base;
+      db.query('DELETE FROM topic WHERE id = ?',[post.id],function(error,result){
+        if(error){
+          throw error;
+        }
+        //error가 안나고 삭제가 정상적으로 되었다면 홈페이지로 이동)localhost:3000
+        response.writeHead(302, {Location: `/`});
         response.end();
-      })
+      });
+      // fs.unlink(`data/${filteredId}`, function(error) {
+      //   response.writeHead(302, {
+      //     Location: `/`
+      //   });
+      //   response.end();})
     });
   } else {
     response.writeHead(404);
